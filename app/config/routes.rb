2aed98ec60823e61b7c6fa96ee2b48ea44f997dd
Nick Freeman <nick.freemn@gmail.com>
2018-07-05 22:31:30 +0000

    Wrap registration form into bootstrap container Add new routes to login and registration pages

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'pages#home'
+
+  devise_scope :user do
+    get 'registration', to: 'devise/registrations#new'
+    get 'login', to: 'devise/sessions#new'
+  end
 end
